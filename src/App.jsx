7f3040c65163b30b866f1650ee74c2bdddc45e92
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-27 18:07:12 +0100

    ADDED: footer bar e spostata la cartella components dentro la cartella src

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Main from "../components/hero/Hero.jsx";
-import Header from "../components/header/Header";
-import Footer from "../components/footer";
+import Main from "./components/hero/Hero.jsx";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
